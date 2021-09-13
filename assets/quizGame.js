0888f09ec0ea592fb5462ae2809137e92a2bc5f3
Andrew Haight <69359454+athaight@users.noreply.github.com>
2021-09-13 07:44:42 -0700

    Update quizGame.js

--- assets/quizGame.js
+++ assets/quizGame.js
@@ -159,7 +159,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > totalQuestions) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/highScores.html')
+        return window.location.assign('./highScores.html')
 
     }
     
